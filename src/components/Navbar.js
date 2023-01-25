1fd41208043566fb364bcfd034f9b2fcb5d01ccb
D <Dickerson.Morgan@outlook.com>
2023-01-25 12:23:40 -0500

    restyled the header. added a hamburger menu for mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,27 @@
 import React from "react";
 import "../styles/Navbar.css";
+
+import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
-export default function Nvbar({ handlePageChange }) {
-    return (
-        <Nav className="navbar">
-            <Nav defaultActiveKey="#About" as="ul">
-                <Navbar.Brand className="justify-content-start">
-                    <Navbar.Text className="headerName">Morgan D</Navbar.Text>
-                </Navbar.Brand>
-
-                <Nav.Item as="li">
-                    <Nav.Link href="#About" onClick={() => handlePageChange("About")}>About</Nav.Link>
-                </Nav.Item>
-                <Nav.Item as="li">
-                    <Nav.Link href="#Portfolio" onClick={() => handlePageChange("Portfolio")}>Portfolio</Nav.Link>
-                </Nav.Item>
-                <Nav.Item as="li">
-                    <Nav.Link href="#Contact" onClick={() => handlePageChange("Contact")}>Contact</Nav.Link>
-                </Nav.Item>
-                <Nav.Item as="li">
-                    <Nav.Link href="#Resume" onClick={() => handlePageChange("Resume")}>Resume</Nav.Link>
-                </Nav.Item>
-            </Nav>
-        </Nav>
-    );
+function BasicExample({handlePageChange}) {
+  return (
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand>M. Dickerson</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#About" onClick={() => handlePageChange("About")}>About</Nav.Link>
+            <Nav.Link href="#Contact" onClick={() => handlePageChange("Contact")}>Contact</Nav.Link>
+            <Nav.Link href="#Portfolio" onClick={() => handlePageChange("Portfolio")}>Portfolio</Nav.Link>
+            <Nav.Link href="#Resume" onClick={() => handlePageChange("Resume")}>Resume</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
+
+export default BasicExample;
