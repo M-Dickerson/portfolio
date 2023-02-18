7714e334051ba3bd1f7a3098b2e151593c174cf7
D <dickerson.morgan@outlook.com>
2023-02-18 17:07:58 -0500

    started adding descriptions to portfolio cards

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -19,7 +19,7 @@ export default function Portfolio() {
                             <Card.Img className="image" src="https://i.imgur.com/r4XKrnY.jpg" alt="a book on a white bedsheet next to coffee and a pair of glasses" />
                             <Card.ImgOverlay>
                                 <Card.Title className="middle text">NY Times Calendar</Card.Title>
-                                <Card.Text className="middle text">Javascript</Card.Text>
+                                <Card.Text className="middle text">An application which allows users to see the best-selling NY Times novels of any date</Card.Text>
                                 <Card.Text className="text middle">
                                     <Button className="button" href="https://github.com/M-Dickerson/nytimes-googlecalendar">GitHub</Button>
                                     <Button className="button" href="https://alexhannani.github.io/nytimes-googlecalendar/">Deployed App</Button>
@@ -37,24 +37,13 @@ export default function Portfolio() {
                                 </Card.Text>
                             </Card.ImgOverlay>
                         </Card>
-                        <Card className="portImage">
-                            <Card.Img className="image" src="https://i.imgur.com/CMYakyj.jpg" alt="no content here yet" />
-                            <Card.ImgOverlay>
-                            <Card.Title className="middle text">Project 3</Card.Title>
-                                <Card.Text className="middle text">This is a placeholder</Card.Text>
-                                <Card.Text className="text middle">
-                                    <Button className="button" href="">GitHub</Button>
-                                    <Button className="button" href="">Deployed App</Button>
-                                </Card.Text>
-                            </Card.ImgOverlay>
-                        </Card>
                     </Col>
                     <Col lg={6}>
                         <Card className="portImage">
                             <Card.Img className="image" src="https://i.imgur.com/vAka5lq.jpg" alt="a border collie running with a ball in its mouth" />
                             <Card.ImgOverlay>
-                                <Card.Title className="middle text">Puppy Play Date!</Card.Title>
-                                <Card.Text className="middle text">Node and Express</Card.Text>
+                                <Card.Title className="middle text">Pet Play Date!</Card.Title>
+                                <Card.Text className="middle text">A full-stack application which allows users to keep track of their animals upcoming playdates</Card.Text>
                                 <Card.Text className="text middle">
                                     <Button className="button" href="https://github.com/M-Dickerson/pet-playdate">GitHub</Button>
                                     <Button className="button" href="https://mighty-harbor-79876.herokuapp.com/">Deployed App</Button>
@@ -65,21 +54,10 @@ export default function Portfolio() {
                             <Card.Img className="image" src="https://i.imgur.com/z4jfvR8.jpg" alt="a purple and orange gradient starry sky" />
                             <Card.ImgOverlay>
                             <Card.Title className="middle text">Journey Journals</Card.Title>
-                                <Card.Text className="middle text">MERN and react</Card.Text>
+                                <Card.Text className="middle text">A full-stack application that allows travelers to post about and share their adventures with others</Card.Text>
                                 <Card.Text className="text middle">
                                     <Button className="button" href="https://github.com/M-Dickerson/journey-journals">GitHub</Button>
                                     <Button className="button" href="https://hidden-reef-01487.herokuapp.com/">Deployed App</Button>
-                                </Card.Text>
-                            </Card.ImgOverlay>
-                        </Card>
-                        <Card className="portImage">
-                            <Card.Img className="image" src="https://i.imgur.com/CMYakyj.jpg" alt="no content here yet" />
-                            <Card.ImgOverlay>
-                            <Card.Title className="middle text">Project 3</Card.Title>
-                                <Card.Text className="middle text">This is a placeholder</Card.Text>
-                                <Card.Text className="text middle">
-                                    <Button className="button" href="">GitHub</Button>
-                                    <Button className="button" href="">Deployed App</Button>
                                 </Card.Text>
                             </Card.ImgOverlay>
                         </Card>
