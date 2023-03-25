b00446f307f3db0543a052ac61571c40310ad770
D <dickerson.morgan@outlook.com>
2023-03-25 14:39:19 -0400

    started reworking the about page. added in fake windows

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,20 +2,51 @@ import React from "react";
 import Icon from "../../images/icon2.png";
 import "../../styles/About.css";
 // links for react bootstrap styling
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Accordion, useAccordionButton, Card } from "react-bootstrap";
+
+function CustomToggle({ children, eventKey }) {
+    const decoratedOnClick = useAccordionButton(eventKey, () =>
+        console.log('totally custom!'),
+    );
+
+    return (
+        <h2
+            type="text"
+            style={{ backgroundColor: 'none' }}
+            onClick={decoratedOnClick}
+        >
+            {children}
+        </h2>
+    );
+}
 
 export default function About() {
     return (
         <Container className="aboutContainer">
             <Row>
                 <Col lg={4}>
-                    <img className="aboutImage" src={Icon} alt="me smiling against a yellow background" />
+                    <Accordion defaultActiveKey={['0']} alwaysOpen>
+                        <Card>
+                            <Card.Header className="accordianImage">
+                                <CustomToggle eventKey="0"></CustomToggle><i class="fa-solid fa-window-minimize"></i><i class="fa-regular fa-window-restore"></i><i class="fa-solid fa-xmark"></i>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="0">
+                                <img className="aboutImage" src={Icon} alt="me smiling against a yellow background" />
+                            </Accordion.Collapse>
+                        </Card>
+                    </Accordion>
                 </Col>
                 <Col lg={8}>
-                    <h1>About Me</h1>
-                    <hr></hr>
-                    <p>Hi! My name is Morgan Dickerson and I'm a Full-Stack Web Dev! I used to spend a lot of time editing Tumblr blog themes but I'd never go any further than that out of fear of breaking something. It feels nice not having to worry about that anymore haha! When I'm not coding I'm either creating art or gaming. I've been drawing for roughly 12 years now, give or take, and I mostly do digital art. Fun fact: I actually drew the image to the left! And yes, that is me!</p>
-                    <p>My coding knowledge was very limited before the LPS Coding Bootcamp but thanks to it I'm now all the better! I'm super excited to start this new chapter of my life and start applying the skills I've learned.</p>
+                    <Accordion defaultActiveKey={['0']} alwaysOpen>
+                        <Card>
+                            <Card.Header>
+                                <CustomToggle eventKey="0">About Me</CustomToggle><i class="fa-solid fa-xmark"></i>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="0">
+                                <Card.Body>Hi! My name is Morgan and I'm a Full-Stack Web Dev! I used to spend a lot of time editing Tumblr blog themes but I'd never go any further than that out of fear of breaking something. It feels nice not having to worry about that anymore haha! When I'm not coding I'm either creating art or gaming. I've been drawing for roughly 12 years now, give or take, and I mostly do digital art. Fun fact: I actually drew the image to the left! (And yes, that is me!)</Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                    </Accordion>
                 </Col>
             </Row>
         </Container>
