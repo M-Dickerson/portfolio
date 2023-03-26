f140a773c5e7ccf99b8e306f964d70adef77bb11
D <dickerson.morgan@outlook.com>
2023-03-25 22:50:02 -0400

    reworked the navbar. hamburger menu still works/added proper flexing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,30 @@
 import React from "react";
 import "../styles/Navbar.css";
 
-import { Nav } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 
-export default function Navbar ({handlePageChange}) {
+export default function Navba({ handlePageChange }) {
   return (
     <>
-      <Nav className="justify-content-center" activeKey="/home">
-        <Nav.Item>
-          <Nav.Link href="#About" onClick={() => handlePageChange("About")}>About</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="#Contact" onClick={() => handlePageChange("Contact")}>Contact</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="#Portfolio" onClick={() => handlePageChange("Portfolio")}>Portfolio</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="#Resume" onClick={() => handlePageChange("Resume")}>Resume</Nav.Link>
-        </Nav.Item>
-      </Nav>
+      <Navbar collapseOnSelect expand="lg">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav activeKey="#About">
+            <Nav.Item>
+              <Nav.Link href="#About" onClick={() => handlePageChange("About")}>About</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#Contact" onClick={() => handlePageChange("Contact")}>Contact</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#Portfolio" onClick={() => handlePageChange("Portfolio")}>Portfolio</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#Resume" onClick={() => handlePageChange("Resume")}>Resume</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </>
   );
 }
